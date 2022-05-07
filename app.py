7c120364176087ed261d2abaff7397de11890d57
Brian Gomes <Brian.james.gomes@gmail.com>
2022-05-07 12:09:40 -0500

    updates

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
     
-@app.route('/rick')
-def rick():
-    return render_template('rick.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
